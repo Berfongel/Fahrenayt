e2790bc20d7b8076fbcbf8cf827b6eaf5b7189ae
Berfongel <121492984+Berfongel@users.noreply.github.com>
2022-12-28 14:58:40 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+double fahrenayt_derecesi,cantigrad;
+Console.WriteLine("Fahrenayt derecesini giriniz : ");
+fahrenayt_derecesi = Convert.ToDouble(Console.ReadLine());
+cantigrad = (((fahrenayt_derecesi-32)*5)/9);
+Console.WriteLine("Cantigrad derecesi : " + cantigrad);
